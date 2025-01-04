64c1bae8b39e8f9b5b05240dcb4d6cbed23cae08
Samed1990 <141842905+Samed1990@users.noreply.github.com>
2025-01-04 15:25:14 +0100

    Update CAPTCHA_solver.py

--- CAPTCHA_solver.py
+++ CAPTCHA_solver.py
@@ -87,7 +87,7 @@ with table_col:
         # Add a styled header for "Submitted Solutions" and move it to the right
         st.markdown(
             """
-            <div style="text-align: left; margin-left: 100px;">
+            <div style="text-align: right; margin-right: 50px;">
                 <h3>Submitted Solutions</h3>
             </div>
             """,
